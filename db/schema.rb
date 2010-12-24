54d71368c20e6c56b9b777a2462d779a31e84eea
James Southern <jms301@gmail.com>
2010-12-24 16:12:07 +0000

    update documentations

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,22 @@
+# Copyright (c) 2010 - James Southern
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
@@ -9,19 +28,31 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100926105933) do
+ActiveRecord::Schema.define(:version => 20101223121332) do
 
-  create_table "past_actions", :force => true do |t|
+  create_table "events", :force => true do |t|
     t.string   "description", :null => false
-    t.datetime "started_at",  :null => false
-    t.datetime "ended_at"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.integer  "user_id",     :null => false
+    t.boolean  "awesome"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "plans", :force => true do |t|
+    t.string   "description"
+    t.integer  "parent_id"
+    t.integer  "user_id"
+    t.datetime "deadline"
+    t.text     "note"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
     t.string   "username"
+    t.integer  "current_action_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
